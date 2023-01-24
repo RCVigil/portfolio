4aaa73706ded0bd2535cc8c205d74b91b6f21c33
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2023-01-24 00:26:57 -0300

    Atualizando favico, add logo e ajeitando cards

--- newPortfolio/src/Components/Header.jsx
+++ newPortfolio/src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import "../Styles/Components/Header.sass";
+import pegasusIcon from '../images/pegasus.svg'
 
 import MenuIcon from "../Utils/MenuIcon";
 
@@ -7,6 +8,7 @@ function Header() {
     <header className="container d-flex justify-content-between row allHeader">
       <MenuIcon />
       <div className="col-8 h1DivHeader">
+        <img className="imgPegasus" src={pegasusIcon} alt="logo Pégasus" srcset="" />
         <h1 className="h1Header">CUERVO DEV</h1>
       </div>
     </header>
