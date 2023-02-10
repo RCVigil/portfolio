1e7cb713b0e07b886b27bd658da113294597c50e
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2023-02-10 14:50:42 -0300

    corrigindo posicionamento do componente ProjetosGithub

--- newPortfolio/src/Pages/ProjetosGithub.jsx
+++ newPortfolio/src/Pages/ProjetosGithub.jsx
@@ -63,15 +63,13 @@ const ProjetosGithub = () => {
         </h2>
       </div>
 
-      <div 
-        className="divMainProjetos d-flex col-11 flex-row w-full"
-      >
+      <div className="divMainProjetos d-flex col-11 flex-row w-full">
         {
-          <div className="d-flex col-3">
+          <div className="d-flex col-3 ">
             {!loading ? (
               <Loading />
             ) : (
-              <div className="divUserGit d-flex flex-column align-items-center p-2">
+              <div className="divUserGit d-flex flex-column align-items-center justify-content-between p-2">
                 <h1 className="h1Name text-lg-right lh-sm font-italic text-capitalize text-decoration-none text-monospace mt-4">
                   {userGitHub.name}
                 </h1>
@@ -105,13 +103,15 @@ const ProjetosGithub = () => {
 
                 <hr />
 
-                <p className="card-text text-lg-right lh-sm font-italic text-decoration-none text-monospace fs-6 mt-4">
-                  {`Estou no GitHub desde`}
-                </p>
+                <div className="justify-content-center">
+                  <p className="card-text text-lg-right lh-sm font-italic text-decoration-none text-monospace fs-6 mt-4">
+                    {`Estou no GitHub desde`}
+                  </p>
 
-                <h1 className="card-text text-lg-right lh-sm font-italic text-decoration-none text-monospace fs-5">
-                  {experiency()}
-                </h1>
+                  <h1 className="card-text text-lg-right lh-sm font-italic text-decoration-none text-monospace fs-5">
+                    {experiency()}
+                  </h1>
+                </div>
 
                 <hr />
 
@@ -121,43 +121,48 @@ const ProjetosGithub = () => {
 
                 <hr />
 
-                <p className="card-text text-lg-right lh-sm font-italic text-decoration-none text-monospace fs-6 mt-4">
-                  {`O último updated foi em:`}
-                </p>
+                <div className="justify-content-center">
+                  <p className="card-text text-lg-right lh-sm font-italic text-decoration-none text-monospace fs-6 mt-4">
+                    {`O último updated foi em:`}
+                  </p>
 
-                <h1 className="card-text text-lg-right lh-sm font-italic text-decoration-none text-monospace fs-5 mb-5">
-                  {`${moment(userGitHub.updated_at).format("DD / MM / YYYY")}`}
-                </h1>
-                <p className="card-text text-lg-right lh-sm font-italic text-decoration-none text-monospace fs-6 mt-4">
-                  Quer conhecer todos meus projetos?
-                </p>
-                <button
-                // onClick={() => path.push('/projetosFull')}
-                  className="btnReposit btn btn-outline-info"
-                >
-                  {/* <a
+                  <h1 className="card-text text-lg-right lh-sm font-italic text-decoration-none text-monospace fs-5 mb-5">
+                    {`${moment(userGitHub.updated_at).format(
+                      "DD / MM / YYYY"
+                    )}`}
+                  </h1>
+                </div>
+
+                <div className="justify-content-center">
+                  <p className="card-text text-lg-right lh-sm font-italic text-decoration-none text-monospace fs-6 mt-4">
+                    Quer conhecer todos meus projetos?
+                  </p>
+                  <button
+                    // onClick={() => path.push('/projetosFull')}
+                    className="btnReposit btn btn-outline-info"
+                  >
+                    {/* <a
                     href={ProjetosFullGit}
                     target="_blank"
                   > */}
-                  <Link
-                  to='/projetosFull'
-                  >
-                    Repositório{" "}
-                    <i className="bi bi-pc-display-horizontal">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        className="bi bi-pc-display-horizontal"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M1.5 0A1.5 1.5 0 0 0 0 1.5v7A1.5 1.5 0 0 0 1.5 10H6v1H1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1h-5v-1h4.5A1.5 1.5 0 0 0 16 8.5v-7A1.5 1.5 0 0 0 14.5 0h-13Zm0 1h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5ZM12 12.5a.5.5 0 1 1 1 0 .5.5 0 0 1-1 0Zm2 0a.5.5 0 1 1 1 0 .5.5 0 0 1-1 0ZM1.5 12h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1ZM1 14.25a.25.25 0 0 1 .25-.25h5.5a.25.25 0 1 1 0 .5h-5.5a.25.25 0 0 1-.25-.25Z" />
-                      </svg>
-                    </i>
-                  {/* </a> */}
-                  </Link>
-                </button>
+                    <Link to="/projetosFull">
+                      Repositório{" "}
+                      <i className="bi bi-pc-display-horizontal">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-pc-display-horizontal"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M1.5 0A1.5 1.5 0 0 0 0 1.5v7A1.5 1.5 0 0 0 1.5 10H6v1H1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1h-5v-1h4.5A1.5 1.5 0 0 0 16 8.5v-7A1.5 1.5 0 0 0 14.5 0h-13Zm0 1h13a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .5-.5ZM12 12.5a.5.5 0 1 1 1 0 .5.5 0 0 1-1 0Zm2 0a.5.5 0 1 1 1 0 .5.5 0 0 1-1 0ZM1.5 12h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1ZM1 14.25a.25.25 0 0 1 .25-.25h5.5a.25.25 0 1 1 0 .5h-5.5a.25.25 0 0 1-.25-.25Z" />
+                        </svg>
+                      </i>
+                      {/* </a> */}
+                    </Link>
+                  </button>
+                </div>
               </div>
             )}
           </div>
@@ -172,7 +177,7 @@ const ProjetosGithub = () => {
                 if (proj.private === false) {
                   if (proj.stargazers_count === 1) {
                     return (
-                      <div className="divProjectMap" key={proj.id}>
+                      <div className="divProjectMap d-flex" key={proj.id}>
                         <div className="divCards ">
                           <div className="cardHeader d-flex text-center flex-column ">
                             <h1 className="h1Name text-center text-wrap text-capitalize fs-4 fw-bolder fst-italic font-monospace text-decoration-none">
@@ -191,9 +196,7 @@ const ProjetosGithub = () => {
                               {!proj.homepage ? (
                                 ""
                               ) : (
-                                <button
-                                  className="LinkProject btn btn-outline-primary rounded-pill"
-                                >
+                                <button className="LinkProject btn btn-outline-primary rounded-pill">
                                   <a
                                     className=""
                                     href={`${proj.homepage}`}
