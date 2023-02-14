700a512b3c9fa81a0e97c3700a568f13d9bcce5a
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2023-02-13 23:35:17 -0300

    Finalizando local asideLeft

--- newPortfolio/src/Components/sobrePort.jsx
+++ newPortfolio/src/Components/sobrePort.jsx
@@ -6,14 +6,14 @@ const SobrePort = () => {
   return (
     <div className="divSobre">
       <div className="textoSobre">
-        <h1 className="titleSobre">Desenvolvedor Front-end</h1>
+        <h1 className="titleSobre">Desenvolvedor Full-Stack</h1>
         <p className="user-select-none float-sm-start parSobre">
           Em 2022, apaixonado por Tecnologia, decidi enfrentar novos desafios
           nesta área, pois acredito que a vida é feita de constantes desafios.
           Assim, após conhecer a Trybe, me dispus a estudar com afinco e
           aprender novas ferramentas que me deram embasamento para trilhar novos
           caminhos. Atualmente, estou apto a desempenhar novas funções na área
-          da tecnologia e já estou com o certificado de Front-End.
+          da tecnologia e já estou com o certificado de Front-End e Back-End.
         </p>
       </div>
     </div>
