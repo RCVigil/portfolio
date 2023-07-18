f0527db81f5710acd9ac94ce44e7f6353a72bb16
RCVigil <rodrigo_vigil@yahoo.com.br>
2023-07-18 14:56:50 -0300

    Build

--- newPortfolio/src/Pages/ProjetosGithub.jsx
+++ newPortfolio/src/Pages/ProjetosGithub.jsx
@@ -30,6 +30,7 @@ const ProjetosGithub = () => {
     };
   }, [userGitHub]);
 
+
   const ENDPOINTRepos = import.meta.env.VITE_REACT_APP_ENDPOINT_REPOS;
   const ENDPOINTUser = import.meta.env.VITE_REACT_APP_ENDPOINT_USER;
 
