6493ea1621b7ab8f772d03690a1f91cae5d193ec
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2024-02-22 21:10:57 -0300

    Bug: corrigindo sitemap

--- newPortfolio/src/routers.jsx
+++ newPortfolio/src/routers.jsx
@@ -5,12 +5,16 @@ import Index from "./Components/Index";
 import Error404 from "./Pages/Error404";
 import ProjetosFullGit from "./Pages/ProjetosFullGit/ProjetosFullGit";
 
+import sitemap from "../sitemap.xml"
+
 function RoutersApp() {
   return (
     <Routes>
       <Route exact path="/" element={<Index />} />
 
       <Route exact path="/projetosFull" element={<ProjetosFullGit />} />
+
+      <Route exact path="/sitemap.xml" element={sitemap} />
 
       <Route path="/404" element={<Error404 />} />
 
