62e5471baa4b507f1a5dbea2aefab2105866b83a
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2024-03-20 13:50:35 -0300

    Bug: sitemap.xml

--- newPortfolio/src/routers.jsx
+++ newPortfolio/src/routers.jsx
@@ -3,18 +3,18 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Index from "./Components/Index";
 import Error404 from "./Pages/Error404";
 import ProjetosFullGit from "./Pages/ProjetosFullGit/ProjetosFullGit";
-import Sitemap from "../sitemap.xml"
+import Sitemap from "../sitemap.xml";
 
 function RoutersApp() {
   return (
     <Routes>
+      <Route path="/sitemap.xml" element={<Sitemap />} />
+
       <Route exact path="/" element={<Index />} />
 
       <Route exact path="/projetosFull" element={<ProjetosFullGit />} />
 
       <Route path="/404" element={<Error404 />} />
-
-      <Route path="/sitemap.xml" element={<Sitemap />} />
 
       <Route path="*" element={<Navigate to="/404" />} />
     </Routes>
