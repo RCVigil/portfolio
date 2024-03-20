3258fedf4f23f63d72b71f556ce55319917209bf
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2024-03-20 14:30:30 -0300

    Bug: sitemap

--- newPortfolio/src/routers.jsx
+++ newPortfolio/src/routers.jsx
@@ -1,22 +1,19 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Index from "./Components/Index";
 import Error404 from "./Pages/Error404";
 import ProjetosFullGit from "./Pages/ProjetosFullGit/ProjetosFullGit";
-import Sitemap from "../sitemap.xml";
 
 function RoutersApp() {
   return (
     <Routes>
-      <Route path="/sitemap.xml" element={<Sitemap />} />
-
       <Route exact path="/" element={<Index />} />
 
       <Route exact path="/projetosFull" element={<ProjetosFullGit />} />
 
       <Route path="/404" element={<Error404 />} />
 
-      <Route path="*" element={<Navigate to="/404" />} />
+      {/* <Route path="*" element={<Navigate to="/404" />} /> */}
     </Routes>
   );
 }
--- newPortfolio/vite.config.js
+++ newPortfolio/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+import Sitemap from "vite-plugin-sitemap";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   esbuild: { jsxFragment: 'Fragment' },
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap()
+  ],
   css: {
     sass: {
       additionalData: `@import 'src/Styles/variables.sass*';`
